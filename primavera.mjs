06464c4ca06ff130068f54ff8e08462911c879a5
white-mice <vikramvdurai@gmail.com>
2023-04-02 16:48:17 +0530

    added team scores and attempted to fix json corruption

--- primavera.mjs
+++ primavera.mjs
@@ -3,45 +3,15 @@ import fs from "fs";
 import { Client } from "@bhavjit/khan-api";
 const client = new Client();
 
-// async function main() {
-//     const replies = await client.getAllMessageReplies("ag5zfmtoYW4tYWNhZGVteXJBCxIIVXNlckRhdGEiHmthaWRfMTAzNzkwNDA4MTM5MTE4NzA4MDQ5ODUwNwwLEghGZWVkYmFjaxiAgJOjuLmJCww")
-//     const contestants = [], blacklist = [], teams = ["ninjaz", "astronomers", "leviathans"], regex = {
-//         join: /\$\[\s*("|')[a-zA-Z0-9 ]+("|')\s*,\s*[0-9]\s*,\s*("|')[A-Za-z0-9 ]+("|')\s*\]\$/,
-//         username: /(;|function|=>|\/\/|"|'|`)/,
-//     }
-//     let comments = ''
-//     replies.forEach(reply => {
-//         let string
-//         try {
-//             if(!blacklist.includes(reply.author.kaid) && !regex.username.test(reply.author.nickname)){
-//                 if(regex.join.test(reply.text)) string = JSON.parse(reply.text.match(/\[.+\]/))
-//                 else comments += `//WARNING! join failed: ${reply.author.nickname} | ${reply.author.kaid}\n`
-//             }
-//             else comments += `//WARNING! blacklisted user attempted to join: ${reply.author.nickname} | ${reply.author.kaid}\n`
-//         }
-//         catch (e) { 
-//             comments += `//ERROR! join failed: ${reply.author.nickname} | ${reply.author.kaid}\n`
-//             console.error(e) 
-//         }
-//         if (string) {
-//             if(teams.includes(string[2])){
-//                 contestants.push(`{name: "${reply.author.nickname}", kaid: "${reply.author.kaid}", level: ${Math.min(Math.max(string[1], 0), 3)}, team: "${string[2]}"}`)
-//                 console.log(`${reply.author.nickname} joined successfully.`)
-//             }
-//             else {
-//                 comments += `//WARNING! invalid join code [team]: ${reply.author.nickname} | ${reply.author.kaid} | ${string[2]}\n`
-//             }
-//         }
-//     })
-//     fs.writeFile('./primaveraContestants.js', `var contestants = [${contestants}]\n${comments}`, err => {
-//         if (err) console.error(err)
-//     })
-//     https.get("https://purge.jsdelivr.net/gh/thelegendski/automated@master/primaveraContestants.js");
-// }
-// main()
+let rawContestants = (await (await fetch(
+    "https://raw.githubusercontent.com/thelegendski/automated/main/officialPrimaveraContestants.js"
+    )).text());
 
-let rawContestants = (await (await fetch("https://raw.githubusercontent.com/thelegendski/automated/main/officialPrimaveraContestants.js")).text());
-let parsedContestants = JSON.parse(eval(rawContestants.split("\n")[0].replace("var contestants = ", "JSON.stringify(").concat(")")))
+let parsedContestants = JSON.parse(eval(
+    rawContestants.split("\n")[0]
+        .replace("var contestants = ", "JSON.stringify(")
+        .concat(")")))
+
 let myTeam = parsedContestants.filter(x => x.team == "Aequor");
 
 myTeam = await Promise.all(myTeam.map(async v => {
@@ -49,8 +19,25 @@ myTeam = await Promise.all(myTeam.map(async v => {
     let u = await client.getUser(k);
     return { user: u, pdata: v};
 }));
+// remove dupes
+myTeam = myTeam.filter((v,i,a)=>a.findIndex(v2=>(v2.user.kaid===v.user.kaid))===i)
 
-fs.writeFile('./primaveraContestants.js', `var teamData = \`${JSON.stringify(myTeam)}\`;`, err => {
+let teamRepr = Buffer.from(JSON.stringify(myTeam)).toString("base64");
+
+let scores = (await client.getProgram("5528901184176128")).code
+    .split("\n")
+    .find(v => v.startsWith('                    {name: "aequor", scores: '));
+
+scores = eval(scores.slice(
+    '                    {name: "aequor", scores: '.length, scores.length-2))
+let totalScore = scores.reduce(function (x, y) {
+        return x + y;
+    }, 0);
+
+fs.writeFile('./primaveraContestants.js', `
+var teamRoundScores = [${scores}];
+var teamTotalScore = ${totalScore};
+var teamData = \`${teamRepr}\`;`, err => {
     if (err) console.error(err)
 })
 await fetch("https://purge.jsdelivr.net/gh/white-mice/automated@master/primaveraContestants.js");
